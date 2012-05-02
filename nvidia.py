26356b3878d03b08ff687a3bbf0327b5c83e7784
Paul Wilson <wilsonp@engr.wisc.edu>
2012-05-01 22:49:38 -0500

    A lot of improvements and additions.  Embarking on proper unit testing.

--- nvidia.py
+++ nvidia.py
@@ -1,100 +1,195 @@
-#!/usr/bin/python
+]#!/usr/bin/python
 
 NV_DEFAULT_RESOLUTION="1028x768"
 
 import subprocess
 import re
 
-def get_metamodes():
-    
-    nv_metamode_list = subprocess.check_output(["/home/wilsonp/opt/bin/nv-control-dpy",
-                                                "--print-metamodes"]).splitlines()
+# simple function to run nv-control-dpy
+def nv_control_dpy(args):
+    return subprocess.check_output(["/home/wilsonp/opt/bin/nv-control-dpy"]+args)
 
-    mode_id_RE = re.compile(r".*id=(\d+).*:: (.*)")
-    
-    nv_metamode = dict()
+### GET DISPLAY INFO ###
 
-    for metamode in nv_metamode_list:
-        if (mode_id_RE.match(metamode)):
-            mode_id   = mode_id_RE.match(metamode).group(1)
-            mode_desc = mode_id_RE.match(metamode).group(2)
-            nv_metamode[mode_id] = mode_desc
+# probe the GPU for the currently attached displays
+def update_displays():
 
-    return nv_metamode
+    nv_control_dpy(["--probe-dpys"])
 
-def get_current_metamode():
-    
-    nv_metamode_list = subprocess.check_output(["/home/wilsonp/opt/bin/nv-control-dpy",
-                                                "--print-current-metamode"]).splitlines()
-
-    mode_id_RE = re.compile(r".*id=(\d+).*:: (.*)")
-    
-    for metamode in nv_metamode_list:
-        if (mode_id_RE.match(metamode)):
-            mode_id   = mode_id_RE.match(metamode).group(1)
-
-    return mode_id
-
+# get the list of the currently attached displays
 def get_displays():
     
+    # get the list from nv-control-dpy
     nv_display_list = subprocess.check_output(["/home/wilsonp/opt/bin/nv-control-dpy",
                                                "--get-associated-dpys"]).splitlines()
     
-    dpy_id_RE = re.compile(r" +([A-Za-z0-9-]+) \(0x[0-9a-f]+\)")
+    # set the regexp
+    dpy_id_RE = re.compile(r" +([A-Za-z0-9-]+) \(0x[0-9a-f]+\): (.*)$")
 
-    dpy_list = []
+    # initialize display list
+    dpy_list = {}
 
+    # for each RE match add an entry to the list
     for display in nv_display_list:
         if (dpy_id_RE.match(display)):
-            dpy_list.append(dpy_id_RE.match(display).group(1))
+            dpy_list[dpy_id_RE.match(display).group(1)]=dpy_id_RE.match(display).group(2)
 
+    # return the list of dictionaries
     return dpy_list
 
+### GET METAMODE INFO ###
 
-def create_metamode_string(display_states):
+# get the current list of metamodes
+def get_metamodes():
+    
+    # get the list from nv-control-dpy
+    metamode_list = nv_control_dpy(["--print-metamodes"]).splitlines()
 
-    metamode = ""
+    # set regexp to match
+    mode_id_RE = re.compile(r".*id=(\d+).*:: (.*)")
+    
+    # initialize metamode dictionary
+    metamode = {}
+
+    # for each mode that matches, add it to the dictionary
+    for mode in metamode_list:
+        if (mode_id_RE.match(mode)):
+            metamode[mode_id_RE.match(mode).group(1)] = mode_id_RE.match(mode).group(2)
+
+    return metamode
+
+# get the current metamode
+def get_current_metamode():
+    
+    # get the list from nv-control-dpy
+    nv_metamode_list = nv_control_dpy(["--print-current-metamode"]).splitlines()
+
+    # set the regexp to match
+    mode_id_RE = re.compile(r".*id=(\d+).*:: (.*)")
+    
+    # for each mode that matches, add it to t
+    for metamode in nv_metamode_list:
+        if (mode_id_RE.match(metamode)):
+            return mode_id_RE.match(metamode).group(1)
+
+    # return -1 for no found mode
+    return -1
+
+### TRANSLATE MODE INFO ###
+# ensure metamode dictionary has entry for every display
+def rationalize_metamode_dict(dpy_modes):
+    
+    dpy_modes_new = {}
+
+    # get list of displays
+    dpylist = get_displays()
+
+    # make sure there is one entry per display
+    # and no entries for non-existent displays
+    for dpy_key in dpylist:
+        if dpy_key in dpy_modes:
+            dpy_modes_new[dpy_key] = dpy_modes[dpy_key]
+        else:
+            dpy_modes_new[dpy_key] = {'enabled':False}
+                
+    # return new display=>mode list
+    return dpy_modes_new
+
+# turn metamode string into a dictionary of display modes
+def parse_metamode_string(modeStr):
+    
+    # initialize dictionary
+    dpy_modes = {}
+
+    # setup regexp for match
+    mode_info_RE = re.compile(r"([a-zA-Z0-9-]+): ([0-9]+x[0-9]+) @([0-9]+x[0-9])")
+
+    # split modelist into individual display modes
+    modeList = modeStr.split(", ")
+
+    # add dictionary entry for each display in metamode
+    for dpyMode in modeList:
+        if mode_info_RE.match(dpyMode):
+            reData = mode_info_RE.match(dpyMode)
+            dpy_modes[reData.group(1)] = {'enabled':True,
+                                          'resolution':reData.group(2)}
+    # return dictionary
+    return dpy_modes
+
+# turn dictionary of display modes into a metamode string
+def create_metamode_string(dpy_modes):
+
+    # initialize lists
+    modeStr    = ""
+    modeStrOff = ""
 
     # for each display
-    for display,state in display_states.iteritems():
-        metamode += ", " + display + ": "
-        if state['enabled']:
-            metamode += state['resolution'] + " @" + state['resolution'] + " "
+    for display,modeInfo in dpy_modes.iteritems():
+        if modeInfo['enabled']:
+            # Add to list of turned on modes
+            modeStr    += ", " + display + ": " + modeInfo['resolution'] + " @" + modeInfo['resolution'] + " +0+0"
         else:
-            metamode += "NULL"
+            # Add to list of turned off modes
+            modeStrOff += ", " + display + ": NULL"
+    
+    # concatenate list of modes
+    modeStr += modeStrOff
 
-    return metamode[2:]
+    return modeStr[2:]
 
-print create_metamode_string({'CRT-0':{'enabled':True,'resolution':"1920x1200"},
-                              'DFP-3':{'enabled':True,'resolution':"1600x900"}})
-
+### Modify state ###
 def enable_displays(display_resolution_map):
     
     # get list of displays
-    nvidia_displays=get_displays()
+    displays=get_displays()
 
     # get list of metamodes
-    nvidia_metamodes=get_metamodes()
+    metamodes=get_metamodes()
 
     # loop through list of available displays
     # to create array of display states
-    display_states = {}
-    for display in nvidia_displays:
+    dpy_modes = {}
+    for display in displays:
         if display not in display_resolution_map:
-            display_states[display] = {'enabled':False}
+            dpy_modes[display] = {'enabled':False}
     
     for display, resolution in display_resolution_map.iteritems():
-        display_states[display] = {'enabled':True,'resolution':resolution}
+        dpy_modes[display] = {'enabled':True,'resolution':resolution}
 
     # build metamode string for desired state
-    
+    metaModeStr = create_metamode_string(dpy_modes)
+
     # get the metamode ID for that state
+    mode_id = [k for k, v in metamodes.iteritems() if v == metaModeStr]
 
     # invoke this metamode
-    return display_states
+    return mode_id
 
-
+# short cut to enable a single display
 def enable_single_display(display_name,resolution):
     return enable_displays({display_name:resolution})
 
-print get_displays()
+def hide_this():
+    modeList = get_metamodes()
+    print modeList['114']
+    print [k for k,v in modeList.iteritems() if v == modeList['114']][0]
+    
+    print enable_displays({'DFP-3':'400x300'})
+
+    update_displays()
+    dpylist = get_displays()
+    print dpylist
+    modelist = get_metamodes()
+# print modelist
+    tstMode = create_metamode_string({"DFP-1":{'enabled':True,"resolution":"1200x900"},
+                                      "DFP-3":{'enabled':True,"resolution":"1600x900"}})
+    print tstMode
+    modeDict = parse_metamode_string(tstMode)
+    print modeDict
+    modeDictFill = rationalize_metamode_dict(modeDict)
+    print modeDictFill
+    tstModeFill = create_metamode_string(modeDictFill)
+    print tstModeFill
+
+def test_update():
+    update_displays()
